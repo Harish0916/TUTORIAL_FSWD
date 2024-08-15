23962f31030b6077ff8376280c723ebb584758a7
harish0916 <harish0916ojhacs@gmail.com>
2024-08-15 20:42:17 +0530

    Proj1

---
+++ FRONTEND/JAVASCRIPT/PROJ1/script.js
@@ -0,0 +1,13 @@
+// document.write("<h1>Hello Pushpendra</h1>")
+
+// -----------------
+let mode = document.getElementById("mode")
+
+function day(){
+    mode.style.background = "#d2f8f8"
+    mode.style.color = "black"
+}
+function night(){
+    mode.style.background = "#444442"
+    mode.style.color = "white"
+}
