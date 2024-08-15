d4cfd13e937371fd0cfb2ec37dfea9663d807db4
harish0916 <harish0916ojhacs@gmail.com>
2024-08-15 20:57:41 +0530

    Proj3

---
+++ FRONTEND/JAVASCRIPT/PROJ2/script.js
@@ -0,0 +1,13 @@
+let demo = document.getElementById("demo")
+
+// demo.innerText = "<h1>HELLO, PUSHPENDRA AND SAMEER</h1>"
+
+// demo.innerHTML = "<h1>HELLO, PUSHPENDRA AND SAMEER</h1>"
+
+
+demo.innerHTML = `
+    <div id="center">
+        <h1>APPLE</h1>
+        <button class="btn">Click Me</button>
+        <h1>BANANA</h1>
+    </div>`
