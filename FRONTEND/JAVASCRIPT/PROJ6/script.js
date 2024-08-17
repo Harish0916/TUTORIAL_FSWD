64cc0d1e3efc33e60eac822cf0dc7f9082def5c2
harish0916 <harish0916ojhacs@gmail.com>
2024-08-17 14:17:36 +0530

    function

---
+++ FRONTEND/JAVASCRIPT/PROJ6/script.js
@@ -0,0 +1,80 @@
+// let fname = "Rinkesh"
+// console.log(fname)
+
+// let sname = new String("Pushpendra")
+// console.log(sname)
+
+// -------------
+
+// let num = Symbol(87)
+// console.log(num);
+
+// --------------
+
+// let obj = {
+//     id: 101,
+//     name: "Madhuri"
+// }
+// console.log(obj.id)
+// console.log(obj.name)
+
+//-------------------
+
+// var sum = ()=>{
+//     console.log(12+13)
+// }
+// sum()
+
+// ----------
+
+// let sum = () => 12+13
+// console.log(sum())
+
+// ------------
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// console.log(sum(12,13))
+
+// ---------------
+
+
+// let sum = function(a,b){
+//     return a+b
+// }
+
+// console.log(sum(12,13))
+
+// -----------------
+
+// let sum = (a,b) =>{
+//     return a+b
+// }
+
+// console.log(sum(12,13))
+// --------------
+
+// let sum = (a,b) =>a+b
+
+// console.log(sum(12,13))
+// ----------------
+
+// (function(){
+//     console.log("welcome")
+// })()
+
+// -------------------
+// let xyz = (function(){
+//     console.log("welcome")
+// })
+
+// xyz()
+// ---------------------
+
+let xyz = (function(){
+    console.log("welcome")
+})
+
+xyz()
